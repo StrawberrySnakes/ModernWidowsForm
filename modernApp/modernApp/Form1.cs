13e2d791c308754af7f2b3d98c971b3f8495b008
Dessa Shapiro (RIT Student) <djs9826@rit.edu>
2024-11-20 13:03:02 -0500

    added code functionallity

---
+++ modernApp/modernApp/Form1.cs
@@ -0,0 +1,21 @@
+namespace modernApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
+    }
+}
